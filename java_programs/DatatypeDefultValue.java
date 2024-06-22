369c3766d13286461fae692e844e7407bb2889cd
pravinbarate1 <pravinbarate1@gmail.com>
2024-06-22 23:09:22 +0530

    java notes upload and program upload

---
+++ java_programs/DatatypeDefultValue.java
@@ -0,0 +1,27 @@
+class DatatypeDefaultValue{
+
+        static byte by = -129;
+        static short s;
+        static int i;
+        static long l;
+        static float f;
+        static double d;
+        static char c;
+        static boolean b;
+
+    public static void main(String [] strg){
+
+        
+
+        System.out.println("by = "+by);
+        System.out.println("s = "+s);
+        System.out.println("i = "+i);
+        System.out.println("l = "+l);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+        System.out.printf("C = %c",c);
+        System.out.println();
+        System.out.println("b = "+b);
+
+    }
+}
---
+++ java_programs/Test.java
@@ -0,0 +1,10 @@
+class Test{
+    public static void main(String [] args){
+        System.out.println("Hello World...");
+
+
+
+	String str = args[0];
+	String str1 = args[2];
+    }
+}
